be6265f2370d9e15ea4d2d4696fd0c1fd3422ac5
InfinityInfotech <infoinfinfotech@gmail.com>
2021-04-11 14:58:37 +0600

    Initial commit

---
+++ lib/view/screens/HomeScreen.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:maslow/view/utils/Constant.dart';
+import 'package:maslow/view/utils/Routes.dart';
+import 'package:maslow/view/widget/CardCourses.dart';
+import 'package:maslow/view/widget/Header.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchControl = new TextEditingController();
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _searchControl.dispose();
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 100),
+        child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                margin: new EdgeInsets.symmetric(
+                    horizontal: Constants.mainPadding,
+                    vertical: Constants.mainPadding),
+                height: 44,
+                width: 44,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(15),
+                  onTap: () {},
+                  child: Icon(
+                    Icons.menu,
+                    size: 30,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Header(),
+          Padding(
+            padding: EdgeInsets.only(
+                left: Constants.mainPadding,
+                right: Constants.mainPadding,
+                top: Constants.mainPadding),
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: <Widget>[
+                // SizedBox(height: Constants.mainPadding * 2),
+
+                // 1. Welcome User
+                Text(
+                  "Welcome back\nStudent!",
+                  style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white),
+                ),
+
+                SizedBox(height: Constants.mainPadding * 2),
+
+                // 3. Start Learning Button Section
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(30.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        color: Color(0xFFFEF3F3),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Start Learning \nNew Stuff!",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Constants.textDark),
+                          ),
+                          SizedBox(height: 10.0),
+                          // Categories Button
+                          Container(
+                            width: 150,
+                            child: ElevatedButton.icon(
+                                onPressed: () {
+                                  // Navigate to Learning
+                                  debugPrint("Pressed here");
+                                  Navigator.pushNamed(
+                                      context, Routes.ROUTE_ALL_COURSE_PAGE);
+                                },
+                                label: Center(
+                                  child: Text(
+                                    "All Courses",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                icon: Icon(Icons.arrow_forward,
+                                    color: Colors.white, size: 16),
+                                style: ElevatedButton.styleFrom(
+                                  shape: new RoundedRectangleBorder(
+                                    borderRadius: new BorderRadius.all(
+                                        Radius.circular(13.0)),
+                                  ),
+                                  primary: Color(0xfff56c6b),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Image Researching Girl
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: Image.asset(
+                        "assets/images/researching.png",
+                        width: 200,
+                        height: 104,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 20.0),
+
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: Text("Courses in progress",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
+                  ),
+                ),
+
+                SizedBox(height: 20.0),
+
+                // List of courses
+                ListView(
+                  scrollDirection: Axis.vertical,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    CardCourses(
+                      image: Image.asset("assets/images/icon_1.png",
+                          width: 40, height: 40),
+                      color: Constants.lightPink,
+                      title: "Adobe XD Prototyping",
+                      hours: "10 hours, 19 lessons",
+                      progress: "25%",
+                      percentage: 0.25,
+                    ),
+                    CardCourses(
+                      image: Image.asset("assets/images/icon_2.png",
+                          width: 40, height: 40),
+                      color: Constants.lightYellow,
+                      title: "Sketch shortcuts and tricks",
+                      hours: "10 hours, 19 lessons",
+                      progress: "50%",
+                      percentage: 0.5,
+                    ),
+                    CardCourses(
+                      image: Image.asset("assets/images/icon_3.png",
+                          width: 40, height: 40),
+                      color: Constants.lightViolet,
+                      title: "UI Motion Design in After Effects",
+                      hours: "10 hours, 19 lessons",
+                      progress: "75%",
+                      percentage: 0.75,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
